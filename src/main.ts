7133e4670e939d9b694b40719cec40d5b14016c1
Jack Richardson <jack.richardson@aptsolutions.net>
2022-07-23 15:08:59 +0100

    init commit & adding dist subtree

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
